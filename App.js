e3944f9e8c7491814ddafec5425ae578943c073a
Yasir Akbar <yasir.akbar86@hotmail.com>
2020-09-15 14:31:08 +0500

    Welcome/View screen style added

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import { View } from "react-native";
 import WelcomeScreen from "./app/screen/WelcomeScreen"
+import ViewImageScreen from "./app/screen/ViewImageScreen"
 
 export default function App() {
   return (
-    <WelcomeScreen />
+   
+      
+     <WelcomeScreen />
+    
   );
 }
 
--- app/screen/WelcomeScreen.js
+++ app/screen/WelcomeScreen.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StyleSheet, ImageBackground, View, Image } from "react-native";
+import { StyleSheet, ImageBackground, View, Image, Text } from "react-native";
 
 function WelcomeScreen(props) {
   return (
@@ -7,9 +7,16 @@ function WelcomeScreen(props) {
       style={styles.background}
       source={require("../assets/background.jpg")}
     >
-      <Image style={styles.logo} source={require("../assets/logo-red.png")} />
-      <View style={styles.loginButton}></View>
-      <View style={styles.registerButton}></View>
+      <View style={styles.logoContainer}>
+        <Image style={styles.logo} source={require("../assets/logo-red.png")} />
+        <Text style={styles.text}>Sell What You Don't Need</Text>
+      </View>
+      <View style={styles.loginButton}>
+        <Text style={styles.buttonText}>Login</Text>
+      </View>
+      <View style={styles.registerButton}>
+        <Text style={styles.buttonText}>Register</Text>
+      </View>
     </ImageBackground>
   );
 }
@@ -23,20 +30,33 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
   loginButton: {
+    alignItems: "center",
+    justifyContent: "center",
     width: "100%",
     height: 70,
     backgroundColor: "#fc5c65",
   },
   registerButton: {
+    alignItems: "center",
+    justifyContent: "center",
     width: "100%",
     height: 70,
     backgroundColor: "#4ecdc4",
   },
+  buttonText: {
+    fontWeight: "bold",
+    fontSize: 20,
+  },
   logo: {
-      width: 100,
-      height: 100,
-      position: "absolute",
-      top: 70,
-
-  }
+    width: 100,
+    height: 100,
+  },
+  logoContainer: {
+    position: "absolute",
+    top: 70,
+    alignItems: "center",
+  },
+  text: {
+    color: "darkgreen",
+  },
 });
